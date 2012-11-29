efdd3c4606011501110c1e211fe75e2157d274ed
7heaven <planet.axis@gmail.com>
2012-11-29 06:22:52 +0200

    Removed unneeded config.php autoloader configuration

--- config/config.php
+++ config/config.php
@@ -1,15 +1,5 @@
 <?php
 
-$autoloader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
-$autoloader->registerNamespace('Axis\\S1\\ServiceContainer', __DIR__.'/../lib/');
-
-$vendorDir = __DIR__.'/../lib/vendor';
-$baseDir = dirname(dirname($vendorDir));
-
-$map = require $vendorDir . '/composer/autoload_namespaces.php';
-$autoloader->registerNamespaces($map);
-$autoloader->register(true);
-
-$classMap = require $vendorDir . '/composer/autoload_classmap.php';
-$autoloader = new \Symfony\Component\ClassLoader\MapClassLoader($classMap);
-$autoloader->register(true);
+//$autoloader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
+//$autoloader->registerNamespace('Axis\\S1\\ServiceContainer', __DIR__.'/../lib/');
+//$autoloader->register(true);
